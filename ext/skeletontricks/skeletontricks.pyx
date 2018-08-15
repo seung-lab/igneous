de9472eed3160f788990ca81c06c0f81adb815cf
William Silversmith <william.silversmith@gmail.com>
2018-08-14 20:12:41 -0400

    fix: several bugs regarding boolean casting in skeletontricks and others

--- ext/skeletontricks/skeletontricks.pyx
+++ ext/skeletontricks/skeletontricks.pyx
@@ -10,8 +10,9 @@ Date: August 2018
 
 from libc.stdlib cimport calloc, free
 from libc.stdint cimport (
-  uint32_t, int8_t, int16_t, int32_t, int64_t
+  uint32_t, uint8_t, int8_t, int16_t, int32_t, int64_t
 )
+from libcpp cimport bool
 from cpython cimport array 
 import array
 import sys
@@ -23,21 +24,73 @@ import numpy as np
 cdef extern from "math.h":
   float INFINITY
 
-def roll_invalidation_ball(dbf, path, float scale, float const):
+def first_label(cnp.ndarray[uint8_t, cast=True, ndim=3] labels):
   cdef int sx, sy, sz 
-  cdef float[:,:,:] dbfv = dbf
+  cdef int  x,  y,  z
 
-  sx = dbf.shape[0]
-  sy = dbf.shape[1]
-  sz = dbf.shape[2]
+  sx = labels.shape[0]
+  sy = labels.shape[1]
+  sz = labels.shape[2]
+
+  for x in range(0, sx):
+    for y in range(0, sy):
+      for z in range(0, sz):
+        if labels[x,y,z]:
+          return (x,y,z)
+
+  return None
+
+def find_target(
+    cnp.ndarray[uint8_t, cast=True, ndim=3] labels, 
+    cnp.ndarray[float, ndim=3] PDRF
+  ):
+  cdef int x,y,z
+  cdef sx, sy, sz
+
+  sx = labels.shape[0]
+  sy = labels.shape[1]
+  sz = labels.shape[2]
+
+  cdef int mx, my, mz
+
+  mx = -1
+  my = -1
+  mz = -1
+
+  cdef float maxpdrf = -INFINITY
+  for x in range(0, sx):
+    for y in range(0, sy):
+      for z in range(0, sz):
+        if labels[x,y,z] and PDRF[x,y,z] > maxpdrf:
+          maxpdrf = PDRF[x,y,z]
+          mx = x
+          my = y
+          mz = z
+
+  return (mx, my, mz)
+
+
+def roll_invalidation_ball(
+    cnp.ndarray[uint8_t, cast=True, ndim=3] labels, 
+    cnp.ndarray[uint32_t, ndim=2] path, 
+    float scale, float const
+  ):
+  
+  cdef int sx, sy, sz 
+
+  sx = labels.shape[0]
+  sy = labels.shape[1]
+  sz = labels.shape[2]
   
   cdef float radius, dist
   cdef int minx, maxx, miny, maxy, minz, maxz
 
   cdef int x,y,z
 
+  cdef int invalidated = 0
+
   for coord in path:
-    radius = dbfv[coord[0], coord[1], coord[2]] * scale + const
+    radius = labels[coord[0], coord[1], coord[2]] * scale + const
 
     minx = max(0, coord[0] - radius)
     maxx = min(sx - 1, coord[0] + radius)
@@ -52,10 +105,11 @@ def roll_invalidation_ball(dbf, path, float scale, float const):
       for y in (miny, maxy):
         for z in (minz, maxz):
           dist = (x - coord[0]) ** 2 + (y - coord[1]) ** 2 + (z - coord[2]) ** 2
-          if (dist <= radius):
-            dbfv[x,y,z] = INFINITY
+          if dist <= radius and labels[x,y,z]:
+            invalidated += 1
+            labels[x,y,z] = 0
 
-  return dbf
+  return invalidated
 
 
 

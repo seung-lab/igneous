559c7eb230347df88d322574a5833c2043139215
William Silversmith <william.silversmith@gmail.com>
2018-08-14 20:42:04 -0400

    feat: Cython array remap code (#14)
    * feat: Cython array remap code
    
    * docs: added compilation command for remap

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ import setuptools
 # NOTE: If _mesher.cpp does not exist, you must run
 # cython --cplus -I./ext/third_party/zi_lib/ ./ext/third_party/mc/_mesher.pyx
 
+# NOTE: If fastremap.cpp does not exist, you must run
+# cython --cplus ./ext/remap/fastremap.pyx
+
 third_party_dir = './ext/third_party'
 
 setuptools.setup(
@@ -23,6 +26,15 @@ setuptools.setup(
             include_dirs=[ os.path.join(third_party_dir, name) for name in ('zi_lib/', 'mc/') ],
             extra_compile_args=[
               '-std=c++11','-O3'
+            ]), #don't use  '-fvisibility=hidden', python can't see init module
+        setuptools.Extension(
+            'fastremap',
+            sources=[ './ext/remap/fastremap.cpp' ],
+            depends=[],
+            language='c++',
+            include_dirs=[ ],
+            extra_compile_args=[
+              '-std=c++11', '-O3'
             ]) #don't use  '-fvisibility=hidden', python can't see init module
     ],
 )

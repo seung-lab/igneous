fix: roll_invalidation_ball wasn't scaling based on DBF

igneous/tasks/skeletonization/skeletonization.py
@@ -67,21 +67,15 @@ def TEASAR(DBF, parameters):
 
   paths = []
   valid_labels = np.count_nonzero(labels)
-
-  # xy_path_projection([], labels, 0)
-  i = 1
+  
   while valid_labels > 0:
     target = igneous.skeletontricks.find_target(labels, PDRF)
     path = igneous.dijkstra.dijkstra(np.asfortranarray(PDRF), root, target)
     invalidated, labels = igneous.skeletontricks.roll_invalidation_ball(
-      labels, path, parameters[0], parameters[1]
+      labels, DBF, path, parameters[0], parameters[1]
     )
     valid_labels -= invalidated
     paths.append(path)
-    # xy_path_projection(path, labels, i)
-    i += 1
-
-  # xy_path_projection(paths, labels, i)
 
   skel_verts, skel_edges = path_union(paths)
   skel_radii = DBF[skel_verts[::3], skel_verts[1::3], skel_verts[2::3]]
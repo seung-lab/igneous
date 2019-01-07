9c7a085db0b136d71c363dea746a6a290bec7486
William Silversmith <william.silversmith@gmail.com>
2019-01-07 18:23:57 -0500

    refactor: use connected-components-3d from PyPI
    Released connected components as a PyPI package.

--- setup.py
+++ setup.py
@@ -37,15 +37,6 @@ setuptools.setup(
         '-std=c++11','-O3'
       ]), # don't use  '-fvisibility=hidden', python can't see init module
     setuptools.Extension(
-      'igneous.cc3d',
-      sources=[ './ext/connected_components/cc3d.cpp' ],
-      language='c++',
-      include_dirs=[ np.get_include() ],
-      extra_compile_args=[
-        '-std=c++11', '-O3', '-ffast-math'
-      ]
-    ),
-    setuptools.Extension(
       'igneous.skeletontricks',
       sources=[ './ext/skeletontricks/skeletontricks.cpp' ],
       language='c++',

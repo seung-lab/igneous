7b02c20c17e4671b58653eb5da481f01fa0040bf
William Silversmith <william.silversmith@gmail.com>
2019-02-21 23:00:01 -0500

    refactor: convert all remaining tasks to iterators

--- igneous/task_creation.py
+++ igneous/task_creation.py
@@ -32,17 +32,18 @@ from igneous.tasks import (
 # from igneous.tasks import BigArrayTask
 
 # for provenance files
-try:
-  OPERATOR_CONTACT = subprocess.check_output("git config user.email", shell=True)
-  OPERATOR_CONTACT = str(OPERATOR_CONTACT.rstrip())
-except:
-  try:
-    print(yellow('Unable to determine provenance contact email. Set "git config user.email". Using unix $USER instead.'))
-    OPERATOR_CONTACT = os.environ['USER']
-  except:
-    print(yellow('$USER was not set. The "owner" field of the provenance file will be blank.'))
-    OPERATOR_CONTACT = ''
 
+if __name__ == '__main__':
+  try:
+    OPERATOR_CONTACT = subprocess.check_output("git config user.email", shell=True)
+    OPERATOR_CONTACT = str(OPERATOR_CONTACT.rstrip())
+  except:
+    try:
+      print(yellow('Unable to determine provenance contact email. Set "git config user.email". Using unix $USER instead.'))
+      OPERATOR_CONTACT = os.environ['USER']
+    except:
+      print(yellow('$USER was not set. The "owner" field of the provenance file will be blank.'))
+      OPERATOR_CONTACT = ''
 
 def get_bounds(vol, bounds, shape, mip, chunk_size=None):
   if bounds is None:
@@ -53,38 +54,44 @@ def get_bounds(vol, bounds, shape, mip, chunk_size=None):
     if chunk_size is not None:
       bounds = bounds.expand_to_chunk_size(chunk_size, vol.mip_voxel_offset(mip))
     bounds = Bbox.clamp(bounds, vol.mip_bounds(mip))
-  
 
   print("Volume Bounds: ", vol.mip_bounds(mip))
   print("Selected ROI:  ", bounds)
 
   return bounds
 
-def create_ingest_task(storage, task_queue):
-    """
-    Creates one task for each ingest chunk present in the build folder.
-    It is required that the info file is already placed in order for this task
-    to run succesfully.
-    """
-    for filename in storage.list_files(prefix='build/'):
-        t = IngestTask(
-          chunk_path=storage.get_path_to_file('build/'+filename),
-          chunk_encoding='npz',
-          layer_path=storage.layer_path,
-        )
-        task_queue.insert(t)
+def create_ingest_task(cloudpath):
+  """
+  Creates one task for each ingest chunk present in the build folder.
+  It is required that the info file is already placed in order for this task
+  to run succesfully.
+  """
+  class IngestTaskIterator():
+    def __iter__(self):
+      with Storage(cloudpath) as storage:
+        for filename in storage.list_files(prefix='build/'):
+          yield IngestTask(
+            chunk_path=storage.get_path_to_file('build/'+filename),
+            chunk_encoding='npz',
+            layer_path=cloudpath,
+          )
+  return IngestTaskIterator()
 
-# def create_bigarray_task(storage, task_queue):
-#     """
-#     Creates one task for each bigarray chunk present in the bigarray folder.
-#     These tasks will convert the bigarray chunks into chunks that ingest tasks are able to understand.
-#     """
-#     for filename in tqdm(storage.list_blobs(prefix='bigarray/')):   
-#         t = BigArrayTask(
+# def create_bigarray_task(cloudpath):
+#   """
+#   Creates one task for each bigarray chunk present in the bigarray folder.
+#   These tasks will convert the bigarray chunks into chunks that ingest tasks are able to understand.
+#   """
+#   class BigArrayTaskIterator():
+#     def __iter__(self):    
+#       with Storage(cloudpath) as storage:
+#         for filename in storage.list_blobs(prefix='bigarray/'):
+#           yield BigArrayTask(
 #             chunk_path=storage.get_path_to_file('bigarray/'+filename),
-#             chunk_encoding='npz', #npz_uint8 to convert affinites float32 affinties to uint8
-#             version='{}/{}'.format(storage._path.dataset_name, storage._path.layer_name))
-#         task_queue.insert(t)
+#             chunk_encoding='npz', # npz_uint8 to convert affinites float32 affinties to uint8
+#             version='{}/{}'.format(storage._path.dataset_name, storage._path.layer_name)
+#           )
+#   return BigArrayTaskIterator()
 
 def compute_build_bounding_box(storage, prefix='build/'):
     bboxes = []
@@ -578,36 +585,42 @@ def create_luminance_levels_tasks(
   return LuminanceLevelsTaskIterator()
 
 def create_watershed_remap_tasks(
-  map_path, src_layer_path, dest_layer_path, 
-  shape=Vec(2048, 2048, 64)):
+    map_path, src_layer_path, dest_layer_path, 
+    shape=Vec(2048, 2048, 64)
+  ):
   shape = Vec(*shape)
   vol = CloudVolume(src_layer_path)
 
   create_downsample_scales(dest_layer_path, mip=0, ds_shape=shape)
 
-  for startpt in tqdm(xyzrange( vol.bounds.minpt, vol.bounds.maxpt, shape ), desc="Inserting Remap Tasks"):
-    task = WatershedRemapTask(
-      map_path=map_path,
-      src_path=src_layer_path,
-      dest_path=dest_layer_path,
-      shape=shape.clone(),
-      offset=startpt.clone(),
-    )
-    task_queue.insert(task)
-  task_queue.wait('Uploading Remap Tasks')
-  dvol = CloudVolume(dest_layer_path)
-  dvol.provenance.processing.append({
-    'method': {
-      'task': 'WatershedRemapTask',
-      'src': src_layer_path,
-      'dest': dest_layer_path,
-      'remap_file': map_path,
-      'shape': list(shape),
-    },
-    'by': OPERATOR_CONTACT,
-    'date': strftime('%Y-%m-%d %H:%M %Z'),
-  }) 
-  dvol.commit_provenance()
+  class WatershedRemapTaskIterator():
+    def __len__(self):
+      return int(reduce(operator.mul, np.ceil(bounds.size3() / shape)))
+    def __iter__(self):
+      for startpt in xyzrange( vol.bounds.minpt, vol.bounds.maxpt, shape ):
+        yield WatershedRemapTask(
+          map_path=map_path,
+          src_path=src_layer_path,
+          dest_path=dest_layer_path,
+          shape=shape.clone(),
+          offset=startpt.clone(),
+        )
+    
+      dvol = CloudVolume(dest_layer_path)
+      dvol.provenance.processing.append({
+        'method': {
+          'task': 'WatershedRemapTask',
+          'src': src_layer_path,
+          'dest': dest_layer_path,
+          'remap_file': map_path,
+          'shape': list(shape),
+        },
+        'by': OPERATOR_CONTACT,
+        'date': strftime('%Y-%m-%d %H:%M %Z'),
+      }) 
+      dvol.commit_provenance()
+
+  return WatershedRemapTaskIterator()
 
 def compute_fixup_offsets(vol, points, shape):
   pts = map(np.array, points)
@@ -620,23 +633,30 @@ def compute_fixup_offsets(vol, points, shape):
 
   return map(nearest_offset, pts)
 
-def create_fixup_downsample_tasks(task_queue, layer_path, points, shape=Vec(2048, 2048, 64), mip=0, axis='z'):
+def create_fixup_downsample_tasks(
+    layer_path, points, 
+    shape=Vec(2048, 2048, 64), mip=0, axis='z'
+  ):
   """you can use this to fix black spots from when downsample tasks fail
   by specifying a point inside each black spot.
   """
   vol = CloudVolume(layer_path, mip)
   offsets = compute_fixup_offsets(vol, points, shape)
 
-  for offset in tqdm(offsets, desc="Inserting Corrective Downsample Tasks"):
-    task = DownsampleTask(
-      layer_path=layer_path,
-      mip=mip,
-      shape=shape,
-      offset=offset,
-      axis=axis,
-    )
-    task_queue.insert(task)
-  task_queue.wait('Uploading')
+  class FixupDownsampleTasksIterator():
+    def __len__(self):
+      return len(offsets)
+    def __iter__(self):
+      for offset in offsets:
+        yield DownsampleTask(
+          layer_path=layer_path,
+          mip=mip,
+          shape=shape,
+          offset=offset,
+          axis=axis,
+        )
+
+  return FixupDownsampleTasksIterator()
 
 def create_quantized_affinity_info(src_layer, dest_layer, shape, mip, chunk_size):
   srcvol = CloudVolume(src_layer)
@@ -651,8 +671,8 @@ def create_quantized_affinity_info(src_layer, dest_layer, shape, mip, chunk_size
   return info
 
 def create_quantize_tasks(
-    task_queue, src_layer, dest_layer, 
-    shape, mip=0, fill_missing=False, chunk_size=[64,64,64]
+    src_layer, dest_layer, shape, 
+    mip=0, fill_missing=False, chunk_size=[64,64,64]
   ):
 
   shape = Vec(*shape)
@@ -665,31 +685,36 @@ def create_quantize_tasks(
 
   create_downsample_scales(dest_layer, mip=mip, ds_shape=shape)
 
-  for startpt in tqdm(xyzrange( destvol.bounds.minpt, destvol.bounds.maxpt, shape ), desc="Inserting Quantize Tasks"):
-    task = QuantizeTask(
-      source_layer_path=src_layer,
-      dest_layer_path=dest_layer,
-      shape=shape.tolist(),
-      offset=startpt.tolist(),
-      fill_missing=fill_missing,
-      mip=mip,
-    )
-    task_queue.insert(task)
-  task_queue.wait('Uploading')
-  destvol.provenance.sources = [ src_layer ]
-  destvol.provenance.processing.append({
-    'method': {
-      'task': 'QuantizeTask',
-      'source_layer_path': src_layer,
-      'dest_layer_path': dest_layer,
-      'shape': shape.tolist(),
-      'fill_missing': fill_missing,
-      'mip': mip,
-    },
-    'by': OPERATOR_CONTACT,
-    'date': strftime('%Y-%m-%d %H:%M %Z'),
-  }) 
-  destvol.commit_provenance()
+  class QuantizeTasksIterator():
+    def __len__(self):
+      return  int(reduce(operator.mul, np.ceil(destvol.bounds.size3() / shape)))
+    def __iter__(self):
+      for startpt in xyzrange( destvol.bounds.minpt, destvol.bounds.maxpt, shape ):
+        yield QuantizeTask(
+          source_layer_path=src_layer,
+          dest_layer_path=dest_layer,
+          shape=shape.tolist(),
+          offset=startpt.tolist(),
+          fill_missing=fill_missing,
+          mip=mip,
+        )
+
+      destvol.provenance.sources = [ src_layer ]
+      destvol.provenance.processing.append({
+        'method': {
+          'task': 'QuantizeTask',
+          'source_layer_path': src_layer,
+          'dest_layer_path': dest_layer,
+          'shape': shape.tolist(),
+          'fill_missing': fill_missing,
+          'mip': mip,
+        },
+        'by': OPERATOR_CONTACT,
+        'date': strftime('%Y-%m-%d %H:%M %Z'),
+      }) 
+      destvol.commit_provenance()
+
+  return QuantizeTasksIterator()
 
 # split the work up into ~1000 tasks (magnitude 3)
 def create_mesh_manifest_tasks(layer_path, magnitude=3):
@@ -820,104 +845,115 @@ def create_hypersquare_consensus_tasks(
 
   return HyperSquareConsensusTaskIterator()
 
-def create_mask_affinity_map_tasks(task_queue, aff_input_layer_path, aff_output_layer_path, 
-        aff_mip, mask_layer_path, mask_mip, output_block_start, output_block_size, grid_size ):
+def create_mask_affinity_map_tasks(
+    aff_input_layer_path, aff_output_layer_path, 
+    aff_mip, mask_layer_path, mask_mip, output_block_start, 
+    output_block_size, grid_size 
+  ):
     """
     affinity map masking block by block. The block coordinates should be aligned with 
     cloud storage. 
     """
-    for z in tqdm(range(grid_size[0]), desc='z loop'):
-        for y in range(grid_size[1]):
-            for x in range(grid_size[2]):
-                output_bounds = Bbox.from_slices(tuple(slice(s+x*b, s+x*b+b)
-                        for (s, x, b) in zip(output_block_start, (z, y, x), output_block_size)))
-                task = MaskAffinitymapTask(
-                    aff_input_layer_path=aff_input_layer_path,
-                    aff_output_layer_path=aff_output_layer_path,
-                    aff_mip=aff_mip, 
-                    mask_layer_path=mask_layer_path,
-                    mask_mip=mask_mip,
-                    output_bounds=output_bounds,
-                )
-                task_queue.insert(task)
-    task_queue.wait()
 
-    vol = CloudVolume(output_layer_path, mip=aff_mip)
-    vol.provenance.processing.append({
-        'method': {
-            'task': 'InferenceTask',
-            'aff_input_layer_path': aff_input_layer_path,
-            'aff_output_layer_path': aff_output_layer_path,
-            'aff_mip': aff_mip,
-            'mask_layer_path': mask_layer_path,
-            'mask_mip': mask_mip,
-            'output_block_start': output_block_start,
-            'output_block_size': output_block_size, 
-            'grid_size': grid_size,
-        },
-        'by': OPERATOR_CONTACT,
-        'date': strftime('%Y-%m-%d %H:%M %Z'),
-    })
-    vol.commit_provenance()
+    class MaskAffinityMapTaskIterator():
+      def __len__(self):
+        return int(reduce(operator.mul, grid_size))
+      def __iter__(self):
+        for x, y, z in xyzrange(grid_size):
+          output_bounds = Bbox.from_slices(tuple(slice(s+x*b, s+x*b+b)
+                  for (s, x, b) in zip(output_block_start, (z, y, x), output_block_size)))
+          yield MaskAffinitymapTask(
+              aff_input_layer_path=aff_input_layer_path,
+              aff_output_layer_path=aff_output_layer_path,
+              aff_mip=aff_mip, 
+              mask_layer_path=mask_layer_path,
+              mask_mip=mask_mip,
+              output_bounds=output_bounds,
+          )
+
+        vol = CloudVolume(output_layer_path, mip=aff_mip)
+        vol.provenance.processing.append({
+            'method': {
+                'task': 'InferenceTask',
+                'aff_input_layer_path': aff_input_layer_path,
+                'aff_output_layer_path': aff_output_layer_path,
+                'aff_mip': aff_mip,
+                'mask_layer_path': mask_layer_path,
+                'mask_mip': mask_mip,
+                'output_block_start': output_block_start,
+                'output_block_size': output_block_size, 
+                'grid_size': grid_size,
+            },
+            'by': OPERATOR_CONTACT,
+            'date': strftime('%Y-%m-%d %H:%M %Z'),
+        })
+        vol.commit_provenance()
+
+  return MaskAffinityMapTaskIterator()
 
 
-def create_inference_tasks(task_queue, image_layer_path, convnet_path, 
-        mask_layer_path, output_layer_path, output_block_start, output_block_size, 
-        grid_size, patch_size, patch_overlap, cropping_margin_size,
-        output_key='output', num_output_channels=3, 
-        image_mip=1, output_mip=1, mask_mip=3):
+def create_inference_tasks(
+    image_layer_path, convnet_path, 
+    mask_layer_path, output_layer_path, output_block_start, output_block_size, 
+    grid_size, patch_size, patch_overlap, cropping_margin_size,
+    output_key='output', num_output_channels=3, 
+    image_mip=1, output_mip=1, mask_mip=3
+  ):
     """
     convnet inference block by block. The block coordinates should be aligned with 
     cloud storage. 
     """
-    for z in tqdm(range(grid_size[0]), desc='z loop'):
-        for y in range(grid_size[1]):
-            for x in range(grid_size[2]):
-                output_offset = tuple(s+x*b for (s, x, b) in 
-                                      zip(output_block_start, (z, y, x), 
-                                          output_block_size))
-                task = InferenceTask(
-                    image_layer_path=image_layer_path,
-                    convnet_path=convnet_path,
-                    mask_layer_path=mask_layer_path,
-                    output_layer_path=output_layer_path,
-                    output_offset=output_offset,
-                    output_shape=output_block_size,
-                    patch_size=patch_size, 
-                    patch_overlap=patch_overlap,
-                    cropping_margin_size=cropping_margin_size,
-                    output_key=output_key,
-                    num_output_channels=num_output_channels,
-                    image_mip=image_mip,
-                    output_mip=output_mip,
-                    mask_mip=mask_mip
-                )
-                task_queue.insert(task)
-    task_queue.wait('Uploading InferenceTasks')
+    class InferenceTaskIterator():
+      def __len__(self):
+        return int(reduce(operator.mul, grid_size))
+      def __iter__(self):
+        for x, y, z in xyzrange(grid_size):
+          output_offset = tuple(s+x*b for (s, x, b) in 
+                                zip(output_block_start, (z, y, x), 
+                                    output_block_size))
+          yield InferenceTask(
+              image_layer_path=image_layer_path,
+              convnet_path=convnet_path,
+              mask_layer_path=mask_layer_path,
+              output_layer_path=output_layer_path,
+              output_offset=output_offset,
+              output_shape=output_block_size,
+              patch_size=patch_size, 
+              patch_overlap=patch_overlap,
+              cropping_margin_size=cropping_margin_size,
+              output_key=output_key,
+              num_output_channels=num_output_channels,
+              image_mip=image_mip,
+              output_mip=output_mip,
+              mask_mip=mask_mip
+          )
 
-    vol = CloudVolume(output_layer_path, mip=output_mip)
-    vol.provenance.processing.append({
-        'method': {
-            'task': 'InferenceTask',
-            'image_layer_path': image_layer_path,
-            'convnet_path': convnet_path,
-            'mask_layer_path': mask_layer_path,
-            'output_layer_path': output_layer_path,
-            'output_offset': output_offset,
-            'output_shape': output_block_size,
-            'patch_size': patch_size,
-            'patch_overlap': patch_overlap,
-            'cropping_margin_size': cropping_margin_size,
-            'output_key': output_key,
-            'num_output_channels': num_output_channels,
-            'image_mip': image_mip,
-            'output_mip': output_mip,
-            'mask_mip': mask_mip,
-        },
-        'by': OPERATOR_CONTACT,
-        'date': strftime('%Y-%m-%d %H:%M %Z'),
-    })
-    vol.commit_provenance()
+
+        vol = CloudVolume(output_layer_path, mip=output_mip)
+        vol.provenance.processing.append({
+            'method': {
+                'task': 'InferenceTask',
+                'image_layer_path': image_layer_path,
+                'convnet_path': convnet_path,
+                'mask_layer_path': mask_layer_path,
+                'output_layer_path': output_layer_path,
+                'output_offset': output_offset,
+                'output_shape': output_block_size,
+                'patch_size': patch_size,
+                'patch_overlap': patch_overlap,
+                'cropping_margin_size': cropping_margin_size,
+                'output_key': output_key,
+                'num_output_channels': num_output_channels,
+                'image_mip': image_mip,
+                'output_mip': output_mip,
+                'mask_mip': mask_mip,
+            },
+            'by': OPERATOR_CONTACT,
+            'date': strftime('%Y-%m-%d %H:%M %Z'),
+        })
+        vol.commit_provenance()
+
+  return InferenceTaskIterator()
 
 
 def upload_build_chunks(storage, volume, offset=[0, 0, 0], build_chunk_size=[1024,1024,128]):

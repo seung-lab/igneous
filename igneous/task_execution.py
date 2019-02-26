ddc48b430b16661b85711eeb3c396378fd31d409
William Silversmith <william.silversmith@gmail.com>
2019-02-26 10:50:12 -0500

    fix: missing reference to LOOP

--- igneous/task_execution.py
+++ igneous/task_execution.py
@@ -19,7 +19,7 @@ from igneous.secrets import QUEUE_NAME, QUEUE_TYPE, SQS_URL, LEASE_SECONDS
 @click.option('--queue', default=QUEUE_NAME,  help='Name of pull queue to use.')
 @click.option('--server', default=QUEUE_TYPE,  help='Which queue server to use. (appengine or pull-queue)')
 @click.option('--qurl', default=SQS_URL,  help='SQS Queue URL if using SQS')
-@click.option('--loop/--no-loop', default=LOOP, help='run execution in infinite loop or not', is_flag=True)
+@click.option('--loop/--no-loop', default=True, help='run execution in infinite loop or not', is_flag=True)
 def command(tag, m, queue, server, qurl, loop):
   if not m:
     execute(tag, queue, server, qurl, loop)

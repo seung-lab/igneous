956e8a242e58593a0511218f458ff04576953a49
William Silversmith <william.silversmith@gmail.com>
2019-04-03 23:33:38 -0400

    refactor: rename _mesher to mesher

--- setup.py
+++ setup.py
@@ -21,13 +21,13 @@ setuptools.setup(
     pbr=True,
     ext_modules=[
         setuptools.Extension(
-            'igneous._mesher',
-            sources=[ os.path.join(third_party_dir, name) for name in ('mc/_mesher.cpp','mc/cMesher.cpp') ],
+            'igneous.mesher',
+            sources=[ os.path.join(third_party_dir, name) for name in ('mc/mesher.cpp','mc/cMesher.cpp') ],
             depends=[ os.path.join(third_party_dir, 'mc/cMesher.h')],
             language='c++',
             include_dirs=[ os.path.join(third_party_dir, name) for name in ('zi_lib/', 'mc/') ],
             extra_compile_args=[
-              '-std=c++11','-O3'
+              '-std=c++11','-O3', '-mssse3'
             ]), #don't use  '-fvisibility=hidden', python can't see init module
     ],
 )

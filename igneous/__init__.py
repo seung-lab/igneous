c4014d47cc3ea580c2dec67b2fe46d94d08a9a77
William Silversmith <william.silversmith@gmail.com>
2020-04-21 00:08:26 -0400

    refactor: remove ancient Precomputed shim

--- igneous/__init__.py
+++ igneous/__init__.py
@@ -2,7 +2,3 @@ from zmesh import Mesher
 from taskqueue import MockTaskQueue, TaskQueue, RegisteredTask
 from .tasks import *
 from cloudvolume import CloudVolume, Storage, EmptyVolumeException
-
-def Precomputed(storage, scale_idx=0, fill=False):
-  """Shim to provide backwards compatibility with Precomputed."""
-  return CloudVolume(storage.layer_path, mip=scale_idx, fill_missing=fill)

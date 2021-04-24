2fb3be2f63b3a4437ea6e6f4625ff9f541f4ee03
William Silversmith <william.silversmith@gmail.com>
2021-04-23 20:58:11 -0400

    refactor: remove unnecessary imports

--- igneous/task_creation.py
+++ igneous/task_creation.py
@@ -1,7 +1,4 @@
-from __future__ import print_function
-
 from collections import defaultdict
-from six.moves import range
 from itertools import product
 from functools import reduce, partial
 import operator
@@ -20,7 +17,6 @@ from tqdm import tqdm
 
 import cloudvolume
 from cloudvolume import CloudVolume
-from cloudvolume.storage import Storage, SimpleStorage
 from cloudvolume.lib import Vec, Bbox, max2, min2, xyzrange, find_closest_divisor, yellow, jsonify
 from cloudvolume.datasource.precomputed.sharding import ShardingSpecification
 from cloudfiles import CloudFiles
@@ -759,9 +755,9 @@ def create_meshing_tasks(
     vol.info['mesh'] = mesh_dir
     vol.commit_info()
 
-  stor = SimpleStorage(layer_path)
+  cf = CloudFiles(layer_path)
   info_filename = '{}/info'.format(mesh_dir)
-  mesh_info = stor.get_json(info_filename) or {}
+  mesh_info = cf.get_json(info_filename) or {}
   mesh_info['@type'] = 'neuroglancer_legacy_mesh'
   mesh_info['mip'] = int(vol.mip)
   mesh_info['chunk_size'] = shape.tolist()
@@ -769,7 +765,7 @@ def create_meshing_tasks(
     mesh_info['spatial_index'] = {
         'chunk_size': (shape*vol.resolution).tolist()
     }
-  stor.put_json(info_filename, mesh_info)
+  cf.put_json(info_filename, mesh_info)
 
   class MeshTaskIterator(FinelyDividedTaskIterator):
     def task(self, shape, offset):

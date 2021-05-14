6a65ae0998146771452dca19b87b3a3f9ab8a917
William Silversmith <william.silversmith@gmail.com>
2021-05-13 20:14:33 -0400

    refactor: remove references to SimpleStorage

--- igneous/task_creation.py
+++ igneous/task_creation.py
@@ -18,7 +18,6 @@ from tqdm import tqdm
 
 import cloudvolume
 from cloudvolume import CloudVolume
-from cloudvolume.storage import Storage, SimpleStorage
 from cloudvolume.lib import Vec, Bbox, max2, min2, xyzrange, find_closest_divisor, yellow, jsonify
 from cloudvolume.datasource.precomputed.sharding import ShardingSpecification
 from cloudfiles import CloudFiles
@@ -758,9 +757,9 @@ def create_meshing_tasks(
     vol.info['mesh'] = mesh_dir
     vol.commit_info()
 
-  stor = SimpleStorage(layer_path)
+  cf = CloudFiles(layer_path)
   info_filename = '{}/info'.format(mesh_dir)
-  mesh_info = stor.get_json(info_filename) or {}
+  mesh_info = cf.get_json(info_filename) or {}
   mesh_info['@type'] = 'neuroglancer_legacy_mesh'
   mesh_info['mip'] = int(vol.mip)
   mesh_info['chunk_size'] = shape.tolist()
@@ -769,7 +768,7 @@ def create_meshing_tasks(
         'resolution': vol.resolution.tolist(),
         'chunk_size': (shape*vol.resolution).tolist(),
     }
-  stor.put_json(info_filename, mesh_info)
+  cf.put_json(info_filename, mesh_info)
 
   class MeshTaskIterator(FinelyDividedTaskIterator):
     def task(self, shape, offset):

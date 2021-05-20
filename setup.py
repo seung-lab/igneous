f4aa419427e64027e36933e04ad59cbda772e642
William Silversmith <william.silversmith@gmail.com>
2021-05-19 23:15:27 -0400

    refactor: remove [deflate] extra (it's standard nowadays)

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ import setuptools
 
 setuptools.setup(
   setup_requires=['pbr', 'numpy'],
-  extras_require={
-    "deflate": [ "deflate" ],
-  },
   entry_points={
     "console_scripts": [
       "igneous=igneous_cli:main"

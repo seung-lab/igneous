325ad2c1db195424d6bc5a28b7906ae54092d32d
William Silversmith <william.silversmith@gmail.com>
2021-09-16 15:35:21 -0400

    release(0.8.0): adds --clean-info flag

--- igneous_cli/cli.py
+++ igneous_cli/cli.py
@@ -49,7 +49,7 @@ class Tuple2(click.ParamType):
 
 @click.group()
 @click.option("-p", "--parallel", default=1, help="Run with this number of parallel processes. If 0, use number of cores.")
-@click.version_option(version="0.7.0")
+@click.version_option(version="0.8.0")
 @click.pass_context
 def main(ctx, parallel):
   """

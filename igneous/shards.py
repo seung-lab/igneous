fix(transfer): sharding shape computation did not account for minishard bits
fixtest: the shard test was using an incorrect bounding box

igneous/shards.py
@@ -16,19 +16,23 @@ def image_shard_shape_from_spec(
   chunk_size = Vec(*chunk_size, dtype=np.uint64)
   dataset_size = Vec(*dataset_size, dtype=np.uint64)
   preshift_bits = np.uint64(spec["preshift_bits"])
+  minishard_bits = np.uint64(spec["minishard_bits"])
+  shape_bits = preshift_bits + minishard_bits
 
   grid_size = np.ceil(dataset_size / chunk_size).astype(np.uint64)
   one = np.uint64(1)
 
-  if preshift_bits >= 64:
-    raise ValueError(f"preshift_bits must be < 64. Got: {preshift_bits}")
+  if shape_bits >= 64:
+    raise ValueError(
+      f"preshift_bits ({preshift_bits}) + minishard_bits ({minishard_bits}) must be < 64. Sum: {shape_bits}"
+    )
 
   def compute_shape_bits():
     shape = Vec(0,0,0, dtype=np.uint64)
 
     i = 0
     over = [ False, False, False ]
-    while i < preshift_bits:
+    while i < shape_bits:
       changed = False
       for dim in range(3):
         if 2 ** (shape[dim] + 1) < grid_size[dim] * 2 and not over[dim]:
@@ -38,7 +42,7 @@ def image_shard_shape_from_spec(
           i += 1
           changed = True
 
-        if i >= preshift_bits:
+        if i >= shape_bits:
           return shape
 
       if not changed:

test/test_shards.py
@@ -13,54 +13,71 @@ from cloudvolume.datasource.precomputed.sharding import ShardReader, ShardingSpe
 from igneous.task_creation.image import create_sharded_image_info
 from igneous.shards import image_shard_shape_from_spec
 
-def test_sharded_image_bits():
-  def test_scale(scale):
-    dataset_size = Vec(*scale["size"])
-    chunk_size = Vec(*scale["chunk_sizes"][0])
+def prod(x):
+  return reduce(operator.mul, x, 1)
 
-    spec = create_sharded_image_info( 
-      dataset_size=dataset_size,
-      chunk_size=chunk_size,
-      encoding="jpeg",
-      dtype=np.uint8
-    )
-
-    shape = image_shard_shape_from_spec(
-      spec, dataset_size, chunk_size
-    )
-    shape = lib.min2(shape, dataset_size)
-    bbox = Bbox.from_vec(shape)
-    dataset_bbox = Bbox.from_vec(dataset_size)
-    gpts = list(gridpoints(bbox, dataset_bbox, chunk_size))
-    grid_size = np.ceil(dataset_size / chunk_size).astype(np.int64)
-
-    spec = ShardingSpecification.from_dict(spec)
-    reader = ShardReader(None, None, spec)
-
-    morton_codes = compressed_morton_code(gpts, grid_size)
-    all_same_shard = bool(reduce(lambda a,b: operator.eq(a,b) and a,
-      map(reader.get_filename, morton_codes)
-    ))
-
-    assert all_same_shard
-
-  test_scale({
+SCALES = [
+  {
     'chunk_sizes': [[128, 128, 64]],
     'encoding': 'jpeg',
     'key': '48_48_30',
     'resolution': [48, 48, 30],
     'size': [1536, 1408, 2046],
     'voxel_offset': [0, 0, 0]
-  })
-
-  test_scale({
+  },
+  {
     'chunk_sizes': [[128, 128, 64]],
     'encoding': 'jpeg',
     'key': '24_24_30',
     'resolution': [24, 24, 30],
     'size': [3072, 2816, 2046],
     'voxel_offset': [0, 0, 0]
-  })
+  },
+  {
+    'chunk_sizes': [[128, 128, 20]],
+    'encoding': 'raw',
+    'key': '4_4_40',
+    'resolution': [4, 4, 40],
+    'size': [40960, 40960, 990],
+    'voxel_offset': [69632, 36864, 4855],
+  },
+]
+
+@pytest.mark.parametrize("scale", SCALES)
+def test_sharded_image_bits(scale):
+  dataset_size = Vec(*scale["size"])
+  chunk_size = Vec(*scale["chunk_sizes"][0])
+
+  spec = create_sharded_image_info( 
+    dataset_size=dataset_size,
+    chunk_size=chunk_size,
+    encoding="jpeg",
+    dtype=np.uint8
+  )
+
+  shape = image_shard_shape_from_spec(
+    spec, dataset_size, chunk_size
+  )
+
+  shape = lib.min2(shape, dataset_size)
+  dataset_bbox = Bbox.from_vec(dataset_size)
+  gpts = list(gridpoints(dataset_bbox, dataset_bbox, chunk_size))
+  grid_size = np.ceil(dataset_size / chunk_size).astype(np.int64)
+
+  spec = ShardingSpecification.from_dict(spec)
+  reader = ShardReader(None, None, spec)
+
+  morton_codes = compressed_morton_code(gpts, grid_size)
+  min_num_shards = prod(dataset_size / shape)
+  max_num_shards = prod(np.ceil(dataset_size / shape))
+  
+  assert 0 < min_num_shards <= 2 ** spec.shard_bits
+  assert 0 < max_num_shards <= 2 ** spec.shard_bits
+
+  real_num_shards = len(set(map(reader.get_filename, morton_codes)))
+
+  assert min_num_shards <= real_num_shards <= max_num_shards
+
 
 
 
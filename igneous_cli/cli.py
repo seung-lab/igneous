ba00e6953a9e1fbb91d7399d551fbb56968c14b3
William Silversmith <william.silversmith@gmail.com>
2022-01-03 18:47:40 -0500

    release(1.0.0): Adds mesh xfer command

--- igneous_cli/cli.py
+++ igneous_cli/cli.py
@@ -56,7 +56,7 @@ class Tuple2(click.ParamType):
 
 @click.group()
 @click.option("-p", "--parallel", default=1, help="Run with this number of parallel processes. If 0, use number of cores.")
-@click.version_option(version="0.9.3")
+@click.version_option(version="1.0.0")
 @click.pass_context
 def main(ctx, parallel):
   """

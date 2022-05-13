6bca4de7401cbf059afdae6dd7057d45385b026b
William Silversmith <william.silversmith@gmail.com>
2022-05-13 16:03:15 -0400

    release(4.2.0): support for multi-resolution meshing

--- igneous_cli/cli.py
+++ igneous_cli/cli.py
@@ -56,7 +56,7 @@ class Tuple2(click.ParamType):
 
 @click.group()
 @click.option("-p", "--parallel", default=1, help="Run with this number of parallel processes. If 0, use number of cores.")
-@click.version_option(version="4.1.3")
+@click.version_option(version="4.2.0")
 @click.pass_context
 def main(ctx, parallel):
   """

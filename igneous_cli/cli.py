ac4bd51fd40192c4a62dfa902660721332d8fcd9
William Silversmith <william.silversmith@gmail.com>
2022-11-14 16:33:17 -0500

    release(4.11.0): improvements and fixes to multires mesh transfers

--- igneous_cli/cli.py
+++ igneous_cli/cli.py
@@ -64,7 +64,7 @@ class CloudPath(click.ParamType):
 
 @click.group()
 @click.option("-p", "--parallel", default=1, help="Run with this number of parallel processes. If 0, use number of cores.")
-@click.version_option(version="4.10.0")
+@click.version_option(version="4.11.0")
 @click.pass_context
 def main(ctx, parallel):
   """

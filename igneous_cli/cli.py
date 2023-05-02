0c9a389dc1cc6172ecdb356bee6da2d3bb376d77
William Silversmith <william.silversmith@gmail.com>
2023-05-01 20:43:46 -0400

    release(4.15.2): Fixes unbound local error

--- igneous_cli/cli.py
+++ igneous_cli/cli.py
@@ -97,7 +97,7 @@ def compute_bounds(path, mip, xrange, yrange, zrange):
 
 @click.group()
 @click.option("-p", "--parallel", default=1, help="Run with this number of parallel processes. If 0, use number of cores.")
-@click.version_option(version="4.15.1")
+@click.version_option(version="4.15.2")
 @click.pass_context
 def main(ctx, parallel):
   """

ca74882c4e4d60d61ed96f16bd9374e05cc60565
William Silversmith <william.silversmith@gmail.com>
2023-07-18 18:49:49 -0400

    fix: shards do not recognize voxel offsets so bounds should be expanded

--- igneous/task_creation/image.py
+++ igneous/task_creation/image.py
@@ -521,6 +521,8 @@ def create_image_shard_transfer_tasks(
     bounds = dest_vol.bbox_to_mip(bounds, mip=bounds_mip, to_mip=dest_vol.mip)
     bounds = Bbox.clamp(bounds, dest_vol.bounds)
 
+  bounds = bounds.expand_to_chunk_size(shape) # offset is always zero for shards
+
   class ImageShardTransferTaskIterator(FinelyDividedTaskIterator):
     def task(self, shape, offset):
       return partial(ImageShardTransferTask,

c91b65ad53c657fc6479237ae0dca8086a8c5baa
William Silversmith <william.silversmith@gmail.com>
2024-07-02 21:47:46 -0400

    release(4.25.0): zarr support, downsample method selection

--- igneous_cli/cli.py
+++ igneous_cli/cli.py
@@ -162,7 +162,7 @@ def compute_bounds(path, mip, xrange, yrange, zrange):
 
 @click.group()
 @click.option("-p", "--parallel", default=1, help="Run with this number of parallel processes. If 0, use number of cores.")
-@click.version_option(version="4.24.0")
+@click.version_option(version="4.25.0")
 @click.pass_context
 def main(ctx, parallel):
   """

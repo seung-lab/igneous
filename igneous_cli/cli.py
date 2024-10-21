ca71cc29234a14830f24eddf838585692c45fd10
William Silversmith <william.silversmith@gmail.com>
2024-10-21 19:00:41 -0400

    release(4.25.1): bundle of fixes, including jxl support

--- igneous_cli/cli.py
+++ igneous_cli/cli.py
@@ -167,7 +167,7 @@ def compute_bounds(path, mip, xrange, yrange, zrange):
 
 @click.group()
 @click.option("-p", "--parallel", default=1, help="Run with this number of parallel processes. If 0, use number of cores.")
-@click.version_option(version="4.25.0")
+@click.version_option(version="4.25.1")
 @click.pass_context
 def main(ctx, parallel):
   """

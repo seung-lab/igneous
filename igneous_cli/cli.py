889193140eb28c6421c91909a15d1a5b05fb5504
William Silversmith <william.silversmith@gmail.com>
2025-03-07 15:39:34 -0500

    release(4.28.1): fixes path for skeleton xfer

--- igneous_cli/cli.py
+++ igneous_cli/cli.py
@@ -170,7 +170,7 @@ def compute_bounds(path, mip, xrange, yrange, zrange):
 
 @click.group()
 @click.option("-p", "--parallel", default=1, help="Run with this number of parallel processes. If 0, use number of cores.")
-@click.version_option(version="4.28.0")
+@click.version_option(version="4.28.1")
 @click.pass_context
 def main(ctx, parallel):
   """

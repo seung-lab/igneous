79fc1153001eb16c19e5bd874c050cae1e025af5
William Silversmith <william.silversmith@gmail.com>
2025-07-04 11:36:39 -0400

    feat(cli): adds "igneous queue" submenu
    Copies code from ptq command. Should be more discoverable.

--- igneous_cli/cli.py
+++ igneous_cli/cli.py
@@ -1901,3 +1901,113 @@ def read_array_header(fobj):
   func_name = 'read_array_header_' + '_'.join(str(v) for v in version)
   func = getattr(np.lib.format, func_name)
   return func(fobj)
+
+
+@main.group("queue")
+def queuegroup():
+  """
+  Check status and manipulate task queues.
+  """
+  pass
+
+@queuegroup.command()
+@click.argument("path")
+def rezero(path):
+  """Reset collected statistics for queue."""
+  TaskQueue(normalize_path(path)).rezero()
+
+@queuegroup.command()
+@click.argument("path")
+def status(path):
+  """Print vital statistics for queue."""
+  tq = TaskQueue(normalize_path(path))
+  ins = tq.inserted
+  enq = tq.enqueued
+  comp = tq.completed
+  leased = tq.leased
+
+  if not math.isnan(ins):
+    print(f"Inserted: {ins}")
+
+  if ins > 0:
+    print(f"Enqueued: {enq} ({enq / ins * 100:.1f}% left)")
+    if not math.isnan(comp):
+      print(f"Completed: {comp} ({comp / ins * 100:.1f}%)")
+  else:
+    print(f"Enqueued: {enq} (--% left)")
+    if not math.isnan(comp):
+      print(f"Completed: {comp} (--%)")
+
+  if enq > 0:
+    print(f"Leased: {leased} ({leased / enq * 100:.1f}% of queue)")
+  else:
+    print(f"Leased: {leased} (--%) of queue")
+
+@queuegroup.command()
+@click.argument("path")
+def release(path):
+  """Release all tasks from their leases."""
+  TaskQueue(normalize_path(path)).release_all()
+
+@queuegroup.command()
+@click.argument("src")
+@click.argument("dest")
+def cp(src, dest):
+  """
+  Copy the contents of a queue to another
+  service or location. Do not run this
+  process while a queue is being worked.
+
+  Currently sqs queues are not copiable,
+  but you can copy an fq to sqs. The mv
+  command supports sqs queues.
+  """
+  src = normalize_path(src)
+  dest = normalize_path(dest)
+
+  if get_protocol(src) == "sqs":
+    print("ptq: cp does not support sqs:// as a source.")
+    return
+
+  tqd = TaskQueue(dest)
+  tqs = TaskQueue(src)
+
+  tqd.insert(tqs)
+
+@queuegroup.command()
+@click.argument("src")
+@click.argument("dest")
+def mv(src, dest):
+  """
+  Moves the contents of a queue to another
+  service or location. Do not run this
+  process while a queue is being worked.
+
+  Moving an sqs queue to a file queue
+  may result in duplicated tasks.
+  """
+  src = normalize_path(src)
+  dest = normalize_path(dest)
+
+  tqd = TaskQueue(dest, progress=False)
+  tqs = TaskQueue(src, progress=False)
+
+  total = tqs.enqueued
+  with tqdm(total=total, desc="Moving") as pbar:
+    while True:
+      try:
+        tasks = tqs.lease(num_tasks=10, seconds=10)
+      except QueueEmptyError:
+        break
+
+      tqd.insert(tasks)
+      tqs.delete(tasks)
+      pbar.update(len(tasks))
+
+@queuegroup.command()
+@click.argument("queuepath")
+def purge(queuepath):
+  """Delete all queued messages and zero out queue statistics."""
+  queuepath = normalize_path(queuepath)
+  tq = TaskQueue(queuepath)
+  tq.purge()
